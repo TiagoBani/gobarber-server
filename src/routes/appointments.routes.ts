3f930cfec0304fdf8a91065144354932e040ef52
tiagobani <tiago.bani@yahoo.com.br>
2020-08-24 21:56:33 -0300

    [style]: add prettier

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { isEqual, parseISO, startOfDay } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 
-const appointments: { id: string, provider: string, date: string }[] = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfDay(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
