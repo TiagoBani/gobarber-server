f98cf6883f064705b45c4a87dec7594d965ccff4
tiagobani <tiago.bani@yahoo.com.br>
2020-09-07 11:32:49 -0300

    [feat]: add cors in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors'
 
 import uploadConfig from './config/upload';
 import routes from './routes/index';
@@ -11,6 +12,7 @@ import AppError from './errors/AppError';
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
