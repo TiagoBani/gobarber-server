30334dab357ad3bd1cef404530b721e42901dc6f
tiagobani <tiago.bani@yahoo.com.br>
2020-09-22 05:36:09 -0300

    [feat]: add should not be able to recover a not existing user password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
-// import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -19,6 +19,10 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) throw new AppError('User does not exists.');
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido',
