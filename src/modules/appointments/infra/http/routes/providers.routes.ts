4ab711dd9e3d0835f2bbd1bfbe6d8f1b07dc4ab1
tiagobani <tiago.bani@yahoo.com.br>
2020-10-04 20:17:11 -0300

    [feat]: add routes to availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 providersRouter.use(ensureAuthenticated);
 
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.post(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.post(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
